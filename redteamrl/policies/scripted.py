31012e00738bf1e08c18cc47e3e363981dcd20ea
nicholasg1997 <63173211+nicholasg1997@users.noreply.github.com>
2026-07-14 14:52:50 -0600

    refactor: ensure ScriptedClient actions input is always converted to a list

--- redteamrl/policies/scripted.py
+++ redteamrl/policies/scripted.py
@@ -3,7 +3,7 @@ from redteamrl.types import AttackerAction
 
 class ScriptedClient:
 	def __init__(self, actions: list[AttackerAction]):
-		self._actions = actions
+		self._actions = list(actions)
 		self._i = 0
 
 	def act(self, conversation: list[dict]) -> AttackerAction:
